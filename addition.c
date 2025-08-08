e0bbab35231741bd22ff91f57827ba45e6996a8d
Ved3227 <vedpatel9797@gmail.com>
2025-08-08 10:51:59 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+   int num1,num2,sum;
+   printf("First Number:");
+   scanf("%d", &num1);
+   printf("Second Number:");
+   scanf("%d", &num2);
+
+   sum= num1 + num2;
+   printf("Sum of the Numbers: %d", sum);
+   return 0;
+
+}
