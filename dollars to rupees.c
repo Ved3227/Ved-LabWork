0a1486e9b8c145850559669046e938c76cf69365
Ved3227 <vedpatel9797@gmail.com>
2025-08-14 21:38:36 +0530

    Add files via upload

---
+++ dollars to rupees.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+   float num1,multiply;
+   printf("Amount in dollars:");
+   scanf("%f", &num1);
+
+   multiply= num1 * 48;
+   printf("Amount in rupees: %f", multiply);
+   return 0;
+
+}
